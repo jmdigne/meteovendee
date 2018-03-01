06f2bc61ed338e2c7c12e551088584110a5cd99a
Jean-Marc Digne <jmdigne@gmail.com>
2018-03-01 10:21:05 +0100

    V 0.3 fswebcam capture - top black banner

--- bin/capturewebcam.sh
+++ bin/capturewebcam.sh
@@ -2,7 +2,7 @@
 #
 RACINE=/home/disks/pidrive01/projects/meteovendee
 
-# capture 1 image par minute et renome image suivant pattern
+# capture 1 image par minute et renomme image suivant pattern $(date +%Y%m%d)_$(date +%H%M)_vendeecam.jpg
 fswebcam -r 640x480 --title " VendéeCam à l'Ile d'Elle" --jpeg 95 --font sans:10  \
   --top-banner --banner-colour "#000000" --line-colour  "#FF000000" --text-colour  "#00FFFFFF"  \
   -save $RACINE/captures/webcam.jpg --exec ./renamewebcam.sh  -l 60
